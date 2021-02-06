57db109d049ef76278fcc90ca3f78bfcb09b523f
Evan Sosenko <razorx@evansosenko.com>
2021-02-05 20:28:45 -0800

    Add ds_model test

---
+++ pureskillgg_dsdk/ds_model_test.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-docstring
+# pylint: disable=unused-import
+
+import pytest
+from structlog import get_logger
+
+from .ds_model import create_ds_models
+
+
+def test_no_models():
+    model = create_ds_models(models=[], log=get_logger())
+    assert model is not None
