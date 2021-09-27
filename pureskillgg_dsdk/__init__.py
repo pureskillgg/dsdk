7697826bf48736bcecde1916ddc6d875c9847cba
Bill Freeman <bill@pureskill.gg>
2021-09-27 17:18:01 -0400

    import reader at high level

--- pureskillgg_dsdk/__init__.py
+++ pureskillgg_dsdk/__init__.py
@@ -2,3 +2,6 @@
 Python Data Science Development Kit.
 """
 from .ds_model import create_ds_models
+from .ds_io.reader_s3 import DsReaderS3
+from .ds_io.reader_fs import DsReaderFs
+from .ds_io.game_ds_loader import GameDsLoader
