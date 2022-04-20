f464cefdda88d4efa9a4cbc38bf97ed2b3bc9dd3
Bill Freeman <bill@pureskill.gg>
2022-04-20 15:45:19 -0500

    Remove deprecated usage of pandas' append

--- pureskillgg_dsdk/tome/loader.py
+++ pureskillgg_dsdk/tome/loader.py
@@ -43,13 +43,10 @@ class TomeLoader:
     def get_dataframe(self):
         self._load()
 
-        df = None
-        for page in self.manifest["pages"]:
-            df_temp = self._reader.read_page_dataframe(page)
-            if df is None:
-                df = df_temp
-            else:
-                df = df.append(df_temp)
+        dfs = [
+            self._reader.read_page_dataframe(page) for page in self.manifest["pages"]
+        ]
+        df = pd.concat(dfs)
         return df
 
     def get_keyset(self):
