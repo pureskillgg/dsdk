e6d0a1d24288389bb206552b625de3d9a843a53f
Evan Sosenko <razorx@evansosenko.com>
2022-05-19 19:25:05 -0700

    Fix imports

--- pureskillgg_dsdk/__init__.py
+++ pureskillgg_dsdk/__init__.py
@@ -6,4 +6,4 @@ from .ds_io.reader_s3 import DsReaderS3
 from .ds_io.reader_fs import DsReaderFs
 from .ds_io.game_ds_loader import GameDsLoader
 from .tome import TomeCuratorFs, create_tome_curator
-import .adx
+from .adx import *
