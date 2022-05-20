bde1ae8cfc27c8eba58c5e82f85653be58dbf915
Evan Sosenko <evan@pureskill.gg>
2022-05-19 22:16:03 -0700

    New ADX API (#7)
    * Normalize prefix to use /
    
    * New adx api
    
    * Update reader_fs.py
    
    * Update dataset.py
    
    * Rename adx module
    
    * Fix imports
    
    * Fix use of rev_id
    
    * Fix issues
    
    * Fix more issues
    
    * Move export_revisions to class
    
    * Apply suggestions from code review
    
    Do not use import *
    
    Co-authored-by: Bill Freeman <bill@pureskill.gg>
    
    * Update pureskillgg_dsdk/adx/writer_fs.py
    
    Co-authored-by: Bill Freeman <bill@pureskill.gg>
    
    * Update pureskillgg_dsdk/adx/api.py
    
    Co-authored-by: Bill Freeman <bill@pureskill.gg>
    
    * Simplify vars
    
    * Fix exports
    
    Co-authored-by: Bill Freeman <bill@pureskill.gg>

---
+++ pureskillgg_dsdk/adx/__init__.py
@@ -0,0 +1,8 @@
+from .api import (
+    get_adx_dataset_revisions,
+    download_adx_dataset_revision,
+    export_single_adx_dataset_revision_to_s3,
+    export_multiple_adx_dataset_revisions_to_s3,
+    enable_auto_exporting_adx_dataset_revisions_to_s3,
+    disable_auto_exporting_adx_dataset_revisions_to_s3,
+)
--- pureskillgg_dsdk/exchange/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dataset import DataexchangeDataset
-from .factory import create_dataexchange_dataset, download_dataexchange_dataset_revision
