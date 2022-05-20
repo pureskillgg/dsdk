7cfc116ac243824cb4ec7bd73d8921e41e1a6c5c
Evan Sosenko <evan@pureskill.gg>
2022-05-19 18:32:01 -0700

    New adx api

---
+++ pureskillgg_dsdk/exchange/api.py
@@ -0,0 +1,63 @@
+from .dataset import AdxDataset
+from .writer_fs import AdxDatasetWriterFs
+from .writer_s3 import AdxDatasetWriterS3
+
+
+def get_adx_dataset_revisions(dataset_id, /, *, start_date=None, end_date=None):
+    client = AdxDataset(dataset_id=dataset_id, writer=None)
+    return client.get_revisions(start_date, end_date)
+
+
+def download_adx_dataset_revision(
+    root_path, dataset_id, revision_id=None, /, *, prefix=None
+):
+    writer = AdxDatasetWriterFs(root_path=root_path, prefix=prefix)
+    client = AdxDataset(dataset_id=dataset_id, writer=writer)
+
+    rev_id = revision_id
+    if revision_id is None:
+        rev = client.get_latest_revision()
+        if rev is None:
+            raise RuntimeError("No revisions in dataset")
+        rev_id = rev.Id
+
+    client.export_revision(revision_id)
+
+
+def export_single_adx_dataset_revision_to_s3(
+    bucket, dataset_id, revision_id=None, /, *, prefix=None
+):
+    writer = AdxDatasetWriterS3(bucket=bucket, prefix=prefix)
+    client = AdxDataset(dataset_id=dataset_id, writer=writer)
+
+    rev_id = revision_id
+    if revision_id is None:
+        rev = client.get_latest_revision()
+        if rev is None:
+            raise RuntimeError("No revisions in dataset")
+        rev_id = rev.Id
+
+    client.export_revision(rev_id)
+
+
+def export_multiple_adx_dataset_revisions_to_s3(
+    bucket, dataset_id, /, *, prefix=None, start_date=None, end_date=None
+):
+    writer = AdxDatasetWriterS3(bucket=bucket, prefix=prefix)
+    client = AdxDataset(dataset_id=dataset_id, writer=writer)
+    revisions = client.get_revisions(start_date, end_date)
+    for revision in revisions:
+        client.export_revision(revision["Id"], revision.get("Comment"))
+
+
+def enable_auto_exporting_adx_dataset_revisions_to_s3(
+    bucket, dataset_id, /, *, prefix=None
+):
+    writer = AdxDatasetWriterS3(bucket=bucket, prefix=prefix)
+    client = AdxDataset(dataset_id=dataset_id, writer=writer)
+    client.auto_export_revisions()
+
+
+def disable_auto_exporting_adx_dataset_revisions_to_s3(dataset_id):
+    client = AdxDataset(dataset_id=dataset_id, writer=None)
+    client.delete_all_auto_revision_exports()
--- pureskillgg_dsdk/exchange/dataset.py
+++ pureskillgg_dsdk/exchange/dataset.py
@@ -1,39 +1,62 @@
-import os
-import time
-import urllib
+import dateutil
 
 import structlog
 import boto3
 
 
-class DataexchangeDataset:
-    def __init__(self, *, root_path, prefix=None, dataset_id, log=None):
+class AdxDataset:
+    def __init__(self, *, dataset_id, writer, log=None):
         self._log = log if log is not None else structlog.get_logger()
         self._log = self._log.bind(
-            client="dataexchange_dataset",
-            root_path=root_path,
-            prefix=prefix,
+            client="adx_dataset",
             dataset_id=dataset_id,
         )
         self._client = boto3.client("dataexchange")
         self.dataset_id = dataset_id
-        self.path = get_dataset_path(root_path, prefix)
         self.dataset_name = None
+        self._writer = writer
         self._dataset = None
 
-    def download_revision(self, revision_id=None):
+    def get_latest_revision(self):
         self._init()
-        rev_id = revision_id if revision_id is not None else self._get_latest_revision()
-        self._log.info(
-            "Download Revision: Start", revision_id=rev_id, output_path=self.path
-        )
+        res = self._client.list_data_set_revisions(DataSetId=self.dataset_id)
+        revisions = res.get("Revisions", [])
+        if len(revisions) == 0:
+            return None
+        return revisions[0]
 
-        assets = self._get_assets(rev_id)
-        for asset in assets:
-            self._download_asset(asset)
+    def get_revisions(self, start_date=None, end_date=None, /):
+        self._init()
+        paginator = self._client.get_paginator("list_data_set_revisions")
+        pages = paginator.paginate(DataSetId=self.dataset_id)
+        revisions = []
+        for page in pages:
+            revisions.extend(page["Revisions"])
+        return [
+            rev
+            for rev in revisions
+            if is_date_between(rev["Comment"], start_date, end_date)
+        ]
 
-        self._log.info(
-            "Download Revision: End", revision_id=rev_id, output_path=self.path
+    def export_revision(self, revision_id, comment=None):
+        self._init()
+        log = self._log.bind(revision_id=revision_id, comment=comment)
+        try:
+            log("Export Revision: Start")
+            self._writer.export_revision(self._client, self.dataset_id, revision_id)
+            log("Export Revision: Success")
+        except BaseException as err:
+            log.error("Export Revision: Fail", exec_info=err)
+            raise
+
+    def auto_export_revisions(self):
+        self._init()
+        self._writer.auto_export_revisions(self._client, self.dataset_id)
+
+    def delete_all_auto_revision_exports(self):
+        self._init()
+        raise NotImplementedError(
+            "To delete an auto-export job, select the job you want to delete. Select the Actions menu in the auto-export job destinations section and choose Remove auto-export job destination."
         )
 
     def _init(self):
@@ -42,66 +65,14 @@ class DataexchangeDataset:
 
         res = self._client.get_data_set(DataSetId=self.dataset_id)
         self.dataset_name = res.get("Name")
-        self.path = os.path.join(self.path, self.dataset_name)
-
-    def _get_latest_revision(self):
-        res = self._client.list_data_set_revisions(DataSetId=self.dataset_id)
-        return res.get("Revisions")[0].get("Id")
-
-    def _get_assets(self, revision_id):
-        res = self._client.list_revision_assets(
-            DataSetId=self.dataset_id, RevisionId=revision_id
-        )
-        assets = res.get("Assets")
-        while "NextToken" in res:
-            res = self._client.list_revision_assets(
-                DataSetId=self.dataset_id,
-                RevisionId=revision_id,
-                NextToken=res.get("NextToken"),
-            )
-            assets.extend(res.get("Assets", []))
-        return assets
-
-    def _download_asset(self, asset):
-        asset_name = asset.get("Name")
-        output_path = os.path.join(self.path, (os.sep).join(asset_name.split("/")))
-        output_dir = os.path.dirname(output_path)
-        os.makedirs(output_dir, exist_ok=True)
-
-        if os.path.isfile(output_path):
-            return
-
-        url = self._get_download_asset_signed_url(asset)
-        urllib.request.urlretrieve(url, output_path)
-        self._log.info("Downloaded Asset", path=output_path, asset_name=asset_name)
-
-    def _get_download_asset_signed_url(self, asset):
-        job = self._client.create_job(
-            Type="EXPORT_ASSET_TO_SIGNED_URL",
-            Details={
-                "ExportAssetToSignedUrl": {
-                    "AssetId": asset.get("Id"),
-                    "DataSetId": asset.get("DataSetId"),
-                    "RevisionId": asset.get("RevisionId"),
-                }
-            },
-        )
-
-        job_id = job.get("Id")
-        self._client.start_job(JobId=job_id)
-
-        while True:
-            time.sleep(1)
-            job = self._client.get_job(JobId=job_id)
-
-            if job.get("State") == "ERROR":
-                message = job.get("Errors")[0].get("Message")
-                raise Exception(f"Job {job_id} failed to complete: {message}")
-
-            if job.get("State") == "COMPLETED":
-                return job.get("Details").get("ExportAssetToSignedUrl").get("SignedUrl")
+        self._log = self._log.bind(dataset_name=self.dataset_name)
 
 
-def get_dataset_path(root_path, prefix):
-    parent_path = root_path if prefix is None else os.path.join(root_path, prefix)
-    return parent_path
+def is_date_between(date, start_date, end_date):
+    start = start_date if start_date is not None else dateutil.parser.isoparse("1900")
+    end = end_date if end_date is not None else dateutil.parser.isoparse("4000")
+    try:
+        middle = dateutil.parser.isoparse(date)
+        return start <= middle < end
+    except dateutil.parser.ParserError:
+        return False
