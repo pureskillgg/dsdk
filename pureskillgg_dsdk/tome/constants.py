c77fd982376380672e3e045ddb6228879463d50b
Bill Freeman <bill@pureskill.gg>
2022-06-10 21:26:18 -0500

    Remove unused manifest key function

--- pureskillgg_dsdk/tome/constants.py
+++ pureskillgg_dsdk/tome/constants.py
@@ -2,18 +2,7 @@ import os
 import structlog
 
 
-def get_tome_manifest_key_fs(path, prefix, ds_type, tome_name, is_copied_header=False):
-    return os.path.join(
-        path,
-        prefix if prefix is not None else "",
-        ds_type,
-        tome_name,
-        "header" if is_copied_header else "",
-        "tome",
-    )
-
-
-def get_page_key_fs(path, subtype, page):
+def get_page_path_fs(path, subtype, page):
     return os.path.join(path, page[subtype]["key"])
 
 
@@ -21,3 +10,4 @@ def filter_ds_reader_logs(_, __, event_dict):
     if event_dict.get("client") == "ds_reader_fs":
         raise structlog.DropEvent
     return event_dict
+
