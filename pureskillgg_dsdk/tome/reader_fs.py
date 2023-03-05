76e472b7a2be2b672c23a7b7164291069ff3b15a
Bill Freeman <billfreeman44@yahoo.com>
2023-03-05 15:57:39 -0600

    Typehints and fix linter

--- pureskillgg_dsdk/tome/reader_fs.py
+++ pureskillgg_dsdk/tome/reader_fs.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import Tuple
 import pandas as pd
 from fastparquet import ParquetFile
 import structlog
@@ -43,7 +44,7 @@ class TomeReaderFs:
             )
 
     @property
-    def exists(self):
+    def exists(self) -> bool:
         """If the tome exists"""
         try:
             self.read_manifest()
@@ -54,7 +55,7 @@ class TomeReaderFs:
             raise
         return True
 
-    def read_manifest(self):
+    def read_manifest(self) -> dict:
         self._log.info("Read Manifest: Start")
         file_location = os.path.join(
             self._root_path, add_prefix(self._manifest_key, self._prefix)
@@ -64,15 +65,15 @@ class TomeReaderFs:
             data = rapidjson.loads(file.read())
         return data
 
-    def read_metadata(self):
+    def read_metadata(self) -> dict:
         return {}
 
-    def read_page(self, page):
+    def read_page(self, page) -> Tuple(pd.DataFrame, list):
         dataframe = self.read_page_dataframe(page)
         keyset = self.read_page_keyset(page)
         return dataframe, keyset
 
-    def read_page_keyset(self, page):
+    def read_page_keyset(self, page) -> list:
         key = self._get_page_key("keyset", page)
 
         content_type = page["keyset"]["contentType"]
@@ -83,7 +84,7 @@ class TomeReaderFs:
         df = ParquetFile(key).to_pandas()
         return list(df.iloc[:, 0])
 
-    def read_page_dataframe(self, page):
+    def read_page_dataframe(self, page) -> pd.DataFrame:
         key = self._get_page_key("dataframe", page)
 
         content_type = page["dataframe"]["contentType"]
